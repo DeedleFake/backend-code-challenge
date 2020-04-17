0589e29e84dfe96f6c33700fb6cad2583dc9f0a9
DeedleFake <deedlefake@users.noreply.github.com>
2020-04-17 18:28:58 -0400

    bcc: Add events to rating events table.

--- bcc/ratings.go
+++ bcc/ratings.go
@@ -1,13 +1,18 @@
 package bcc
 
 import (
+	"errors"
 	"fmt"
+	"math"
 
 	"github.com/jmoiron/sqlx"
 )
 
 // RateUser adds a rating to the ratings table.
 func RateUser(db *sqlx.DB, raterID, userID int, rating float64) (err error) {
+	if raterID == userID {
+		return errors.New("not allowed to rate self")
+	}
 	if (rating < 1) || (rating > 5) {
 		return fmt.Errorf("invalid rating %v", rating)
 	}
@@ -27,9 +32,14 @@ func RateUser(db *sqlx.DB, raterID, userID int, rating float64) (err error) {
 		return fmt.Errorf("before: %w", err)
 	}
 
-	_, err = tx.Exec(`INSERT INTO ratings (rater_id, user_id, rating) VALUES ($1, $2, $3)`, raterID, userID, rating)
+	var newRowID int
+	err = tx.QueryRowx(`
+		INSERT INTO ratings (rater_id, user_id, rating)
+		VALUES ($1, $2, $3)
+		RETURNING id
+	`, raterID, userID, rating).Scan(&newRowID)
 	if err != nil {
-		return fmt.Errorf("insert: %w", err)
+		return fmt.Errorf("scan new row: %w", err)
 	}
 
 	after, err := GetRating(tx, userID)
@@ -37,8 +47,11 @@ func RateUser(db *sqlx.DB, raterID, userID int, rating float64) (err error) {
 		return fmt.Errorf("after: %w", err)
 	}
 
-	if (before < 4) && (after >= 4) {
-		// TODO: Insert event for passing 4 stars.
+	if b, a := math.Floor(before), math.Floor(after); b != a {
+		_, err = tx.Exec(`INSERT INTO rating_events (rating_id, before, after) VALUES ($1, $2, $3)`, newRowID, before, after)
+		if err != nil {
+			return fmt.Errorf("insert event: %w", err)
+		}
 	}
 
 	err = tx.Commit()
